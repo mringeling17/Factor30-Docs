c838c56463f8c7c3da42d1e2469bd874b07fed58
Matias <matias.ringeling@mail.udp.cl>
2024-05-10 07:13:52 -0400

    Refactor code to use WiFi and HTTPClient libraries from ESP8266 to WiFi and HTTPClient libraries from Arduino core for esp32

--- Node/Node.ino
+++ Node/Node.ino
@@ -1,5 +1,5 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <ArduinoJson.h> // Incluye la biblioteca ArduinoJson
 #include <Wire.h>
 #include <AHT10.h>
@@ -12,8 +12,6 @@ AHT10 myAHT10(AHT10_ADDRESS_0X38);
 
 // Reemplaza con la URL de tu servidor donde enviarás los datos
 const String serverUrl = "http://192.168.1.115/receive_data";
-
-WiFiClient client;
 
 void setup() {
   Wire.begin();
@@ -50,24 +48,24 @@ void loop() {
   doc["temperature"] = temp;
   doc["humidity"] = hum;
   doc["wind_speed"] = viento;
-  doc["risk_level"] = risk;
   String jsonData;
   serializeJson(doc, jsonData);
 
   // Configura los headers para enviar JSON
-  http.begin(client, serverUrl); // Especifica la URL del request
+  http.begin(serverUrl); // Especifica la URL del request
   http.addHeader("Content-Type", "application/json");
 
   Serial.println("Data Sent");
 
-
   int httpCode = http.POST(jsonData); // Envía la petición POST
 
-  if (httpCode == 200) { // Verifica si la respuesta ha sido recibida
-    Serial.println("Data recieved Succesfully from server");
-    Serial.println("********************");
+  if (httpCode > 0) { // Verifica si la respuesta ha sido recibida correctamente
     String payload = http.getString(); // Obtiene la respuesta
+    Serial.println("Data received successfully from server");
+    Serial.println("********************");
   } else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(http.errorToString(httpCode).c_str());
   }
 
   http.end(); // Cierra la conexión
